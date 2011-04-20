bb31f60912f717a26b1e0a063d8d4e6a317d1fb0
Leo Picado <lpicado@backcountry.com>
2011-04-19 18:11:20 -0600

    Big progress, adding basic styling to all content types

--- assets/js/the-wall.js
+++ assets/js/the-wall.js
@@ -1,14 +1,25 @@
-$(document).ready(function() {
-	/* solicitations */
-	map = {"content-type review":".solicitation.review", "content-type question":".solicitation.question", 
-		   "content-type photo":".solicitation.photo", "content-type video":".solicitation.video"};
-	
-	$('.cta.display-form').click(function(){
-		$(this).hide();
-		$(".solicitation.review").removeClass("collapsed");
-		$('div.step').removeClass("hidden");
+var map = {"content-type review":".solicitation.review", "content-type question":".solicitation.question", "content-type photo":".solicitation.photo", "content-type video":".solicitation.video"};
+var map2 = {"#all-content":"article", "#only-reviews":".hreview", "#only-questions":".question", "#only-photos":".photo", "#only-videos":".video"};	
+
+var takeMeTo = function(){
+	var url = document.location + "";
+	var pos = url.search("#");	
+	if(pos > 0){
+		url = url.slice(pos);
+		$("#wall-content > article").hide();
+		$("#wall-content " + map2[url]).show();
+	}
+};
+
+var expandCommentsForm = function(){
+	$(".add-upc-reply div textarea").click(function(){
+		$(this).parent().parent().removeClass("collapsed");
 	});
+}
+
+$(document).ready(function() {
 	
+	/* solicitations */	
 	$('#content-types a').click(function(event){
 		event.preventDefault();
 		toDisplay = map[$(this).attr('class')];
@@ -16,11 +27,22 @@ $(document).ready(function() {
 		$(toDisplay).removeClass("hidden");
 	});
 	
+	/* expand review form */
+	$('.cta.display-form').click(function(event){
+		event.preventDefault();
+		$(this).hide();
+		$(".solicitation.review").removeClass("collapsed");
+		$('div.step').removeClass("hidden");
+	});
+	
 	/* content */
-	mapa2 = {"#all-content":"article", "#only-reviews":".hreview", "#only-questions":".question", "#only-photos":".photo", "#only-videos":".video"};	
 	$("#wall-content nav a").click(function(event){
 		$("#wall-content > article").hide();
-		$("#wall-content " + mapa2[$(this).attr("href")]).show();
+		$("#wall-content " + map2[$(this).attr("href")]).show();
 	});
+
+	// url filtering of content
+	takeMeTo();
+	expandCommentsForm();
 	
 });
