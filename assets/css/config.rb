78bb1dd2a59bbbba5413447e0fbe84d85c79fa20
Leo Picado <lpicado@backcountry.com>
2011-04-26 17:56:18 -0600

    Aeron!

--- assets/css/config.rb
+++ assets/css/config.rb
@@ -1,9 +1,9 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = ""
-sass_dir = "src"
-images_dir = "/assets/img"
-javascripts_dir = "/assets/js"
+css_dir = "/assets/css/"
+sass_dir = "/assets/css/src/"
+images_dir = "/assets/img/"
+javascripts_dir = "/assets/js/"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
