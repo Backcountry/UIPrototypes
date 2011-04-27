737b5ff293a617c3bdcb3590e443ca0ba97b6c68
Leo Picado <lpicado@backcountry.com>
2011-04-26 18:44:16 -0600

    Gradient working, gotta use compass 0.11

--- assets/css/config.rb
+++ assets/css/config.rb
@@ -1,8 +1,8 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/assets/css/"
-sass_dir = "/assets/css/src/"
+css_dir = ""
+sass_dir = "/src/"
 images_dir = "/assets/img/"
 javascripts_dir = "/assets/js/"
 # To enable relative paths to assets via compass helper functions. Uncomment:
