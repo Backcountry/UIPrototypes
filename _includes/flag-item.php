9642ace1095f030c4df9a7cf127e7d1be4640bc8
Leo Picado <lpicado@backcountry.com>
2011-05-13 09:57:48 -0600

    Syncing.

---
+++ _includes/flag-item.php
@@ -0,0 +1,14 @@
+      <form class="wall-interaction flag-item">
+        <h3>Something rub you the wrong way?</h3>        
+				<h4>Tell us what's wrong:</h4>
+        <div class="textarea-wrapper">
+          <div class="inner">
+            <textarea name="content" placeholder="Write a comment"></textarea>
+          </div>
+        </div>
+        <h4>Thanks for pointing it out. We'll send it immediately to our clean up crew.</h4>
+				<div class="button-row">
+					<a href="#flag" class="button primary flag">Post Your Comment</a>
+					<a href="#flag" class="button cancel">Post Your Comment</a>
+				</div>
+      </form>
