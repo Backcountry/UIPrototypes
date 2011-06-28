890a109779ffc9c102c2ecfaf01fc7f13561c4f6
Leo Picado <leopic@gmail.com>
2011-06-28 16:13:23 -0600

    Pre leaderboard.

--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/css"
-images_dir = "assets/images"
-javascripts_dir = "assets/javascripts"
+http_path = "/UIPrototypes"
+css_dir = "css"
+sass_dir = "src"
+images_dir = "img"
+javascripts_dir = "js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
